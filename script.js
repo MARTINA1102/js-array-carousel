b91b9f2893832d4604f9b45a090fa25d4cdcab02
MARTINA1102 <martinamiccoli11@gmail.com>
2022-10-26 15:02:27 +0200

    esercizio completato

---
+++ script.js
@@ -0,0 +1,55 @@
+const arrImages = [
+'img/01.jpg',
+'img/02.jpg',
+'img/03.jpg',
+'img/04.jpg',
+'img/05.jpg'];
+
+const eleSlider = document.querySelector('.slider');
+const eleBtnLeft = document.querySelector('.btn-left');
+const eleBtnRight = document.querySelector('.btn-right');
+
+// creo i tag immagine nell'html
+for (let i = 0; i < arrImages.length; i++) {
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+
+	eleSlider.append(eleImg);
+}
+
+const listEleImg = document.querySelectorAll('.slider-img'); 
+
+let activeIndex = 0;
+
+// aggiungo gli event listeners ai due bottoni
+eleBtnRight.addEventListener('click', function () {
+	
+	// tolgo la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	// aumento l'active index
+	activeIndex++;
+	// aggiungo la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+
+});
+
+eleBtnLeft.addEventListener('click', function () {
+	// tolgola classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+
+	// diminuisco l'active index
+	activeIndex--;
+
+	// aggiungo la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	
+});
+
+
+
+
