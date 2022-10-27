77221051aa0d556e687f6e58ce99a013f0ac2c86
MARTINA1102 <martinamiccoli11@gmail.com>
2022-10-27 10:38:45 +0200

    bonus 2

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -5,59 +5,70 @@ const arrImages = [
     '../img/04.jpg',
     '../img/05.jpg'];
     
-    const eleSlider = document.querySelector('.slider');
-    const eleBtnLeft = document.querySelector('.btn-left');
-    const eleBtnRight = document.querySelector('.btn-right');
-    
-    // creo i tag immagine nell'html
-    for (let i = 0; i < arrImages.length; i++) {
-        const eleImg = document.createElement('img');
-        eleImg.src = arrImages[i];
-        eleImg.classList.add('slider-img');
-    
-        if (i === 0) {
-            eleImg.classList.add('active');
-        }
-    
-        eleSlider.append(eleImg);
-    }
-    
-    const listEleImg = document.querySelectorAll('.slider-img'); 
-    
-    let activeIndex = 0;
-    
-    // aggiungo gli event listeners ai due bottoni
-    eleBtnRight.addEventListener('click', function () {
-        
-        // tolgo la classe active dall'elemento attivo corrente
-        listEleImg[activeIndex].classList.remove('active');
-        
-        activeIndex++;
-        if(activeIndex===listEleImg){
-            activeIndex= 0;  
-        }
+    const eleSliderViewer = document.querySelector('.slider-viewer');
+const eleSliderThumbs = document.querySelector('.thumbs');
+const eleBtnLeft = document.querySelector('.btn-left');
+const eleBtnRight = document.querySelector('.btn-right');
 
-        
-        // aggiungo la classe active all'elemento successivo
-        listEleImg[activeIndex].classList.add('active');
+// creare i tag immagine nell'html
+for (let i = 0; i < arrImages.length; i++) {
+	// creare i tag immagine che va nella sezione grande .slider-viewer
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+	eleSliderViewer.append(eleImg);
 
-        
-    
-    });
-    
-    eleBtnLeft.addEventListener('click', function () {
-        // tolgola classe active dall'elemento attivo corrente
-        listEleImg[activeIndex].classList.remove('active');
-        if(activeIndex===0){
-            activeIndex= listEleImg-1;  
-        }else{
-            activeIndex--;
-        }
+	// creare i tag immagine che vanno nella sezione .thumbs
+	const eleThumb = document.createElement('img');
+	eleThumb.src = arrImages[i];
+	eleThumb.classList.add('thumb-img');
+	if (i === 0) {
+		eleThumb.classList.add('active');
+	}
+	eleSliderThumbs.append(eleThumb);
+}
+// document.querySelector('.slider-img').classList.add('active');
 
-        // aggiungo la classe active all'elemento successivo
-        listEleImg[activeIndex].classList.add('active');
-        
-    });
+const listEleImg = document.querySelectorAll('.slider-img'); // non e' un array ma qualcosa di simile
+const listThumbs = document.querySelectorAll('.thumb-img');
+
+let activeIndex = 0;
+
+// aggiungere gli event listeners ai due bottoni
+eleBtnRight.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+
+	activeIndex++;
+	if (activeIndex === listEleImg.length) {
+		activeIndex = 0;
+	}
+
+	// aggiungere la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+});
+
+eleBtnLeft.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length;
+	}
+	activeIndex--;
+
+	// aggiungere la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+});
     
     
     
